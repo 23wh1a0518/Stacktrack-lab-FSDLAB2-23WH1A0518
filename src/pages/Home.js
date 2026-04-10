cb00a974fe14126e195d28cb324b11fa7880f10e
diya mandal <23wh1a0518@bvrithyderabad.edu.in>
2026-04-10 21:51:37 +0530

    [INIT] git commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 // TODO: Replace placeholder values with actual student and lab identifiers
-const STUDENT_ID = 'STUDENT_ID_PLACEHOLDER';
-const LAB_ID = 'LAB_ID_PLACEHOLDER';
+const STUDENT_ID = '23WH1A0518';
+const LAB_ID = 'FSDLAB2';
 
 function Home() {
   return (
     <div>
       <h1>Stack Track Lab</h1>
-      <p>Student ID: {STUDENT_ID}</p>
+      <p>student id: {STUDENT_ID}</p>
       <p>Lab ID: {LAB_ID}</p>
 
       {/* TODO: Replace this placeholder with your question set UI */}
